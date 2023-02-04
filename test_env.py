c4151ecd71fa9f66b67aa931e7d73f9f1a40cb7a
Shannon Chen <shannonchen37@gmail.com>
2023-02-04 20:14:38 +0800

    yaml

---
+++ test_env.py
@@ -0,0 +1,10 @@
+import sys
+import cv2
+import sklearn
+print("which python this env use:")
+print("python path:{}".format(sys.executable))
+print("python version:{}".format(sys.version))
+print("=======================================")
+print("opencv-python version:{}".format(cv2.__version__))
+print("scikit-learn version:{}".format(sklearn.__version__))
+
